3c09693b0a57ec3b16933e7ec7f5e712758eccf9
Rabby-sopno <sopno01737@gmail.com>
2020-07-23 14:03:52 +0600

    fixed_itemNumber_negetiv_problem

--- app.js
+++ app.js
@@ -10,30 +10,54 @@ const secondItemMinusBtn = document.getElementById("second-item-minus-btn");
 
 //todo essential functions
 
-//*changing buying item
+//* function of increament
+function incrementingValue(number) {
+  let numContainer = number;
+  numContainer++;
+  return numContainer;
+}
+
+//*function of discrement
+function discrementingValue(number) {
+  let numContainer = number;
+  if (numContainer > 0) numContainer--;
+  else numContainer = 0;
+  return numContainer;
+}
+
+//*function of changing number of buying item
 function ChangingNumberOfBuyingItem(id, incrementOrDecrement) {
   const item = document.getElementById(id);
-  let numberOfBuying = parseInt(item.value);
-  numberOfBuying = numberOfBuying + incrementOrDecrement;
-  item.value = numberOfBuying;
+  const numberOfBuying = parseInt(item.value);
+  const incrementOrDecrementValue = incrementOrDecrement(numberOfBuying);
+  item.value = incrementOrDecrementValue;
 }
+
+/*
+function changingItemStringtoNumber(id) {
+  const item = document.getElementById(id);
+  const itemValuetoNumber = parseInt(item.value);
+  return [itemValuetoNumber, item];
+}
+console.log(changingItemStringtoNumber("num-of-buying-first-item"));
+*/
 
 //todo first plus and minus button handler
 
 firstItemPlusBtn.addEventListener("click", () => {
-  ChangingNumberOfBuyingItem("num-of-buying-first-item", 1);
+  ChangingNumberOfBuyingItem("num-of-buying-first-item", incrementingValue);
 });
 
 firstItemMinusBtn.addEventListener("click", () => {
-  ChangingNumberOfBuyingItem("num-of-buying-first-item", -1);
+  ChangingNumberOfBuyingItem("num-of-buying-first-item", discrementingValue);
 });
 
 //todo second plus and minus button handler
 
-// secondItemPlusBtn.addEventListener("click", () => {
-//   ChangingNumberOfBuyingItem("num-of-buying-second-item", 1);
-// });
+secondItemPlusBtn.addEventListener("click", () => {
+  ChangingNumberOfBuyingItem("num-of-buying-second-item", incrementingValue);
+});
 
-// secondItemMinusBtn.addEventListener("click", () => {
-//   ChangingNumberOfBuyingItem("num-of-buying-second-item", -1);
-// });
+secondItemMinusBtn.addEventListener("click", () => {
+  ChangingNumberOfBuyingItem("num-of-buying-second-item", discrementingValue);
+});
